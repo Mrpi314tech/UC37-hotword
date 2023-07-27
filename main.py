58fb10baea91d3dc24f4b045f8f4f2eafed4c1f4
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2023-07-27 17:13:31 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import struct
+import pyaudio
+import pvporcupine
+porcupine = None
+pa = None
+audio_stream = None
+
+try:
+    porcupine = pvporcupine.create(keyword_paths=["~/UC37-hotword/U-C-thirty-seven.ppn"], access_key="oCSOG+CGD56VT1OF60VxMxi1CndRSG4K/6em7V63GQAQIeiIaYthCA==")
+
+    pa = pyaudio.PyAudio()
+
+    audio_stream = pa.open(
+                    rate=porcupine.sample_rate,
+                    channels=1,
+                    format=pyaudio.paInt16,
+                    input=True,
+                    frames_per_buffer=porcupine.frame_length)
+
+    while True:
+        pcm = audio_stream.read(porcupine.frame_length)
+        pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+
+        keyword_index = porcupine.process(pcm)
+        if keyword_index >= 0:
+            os.system('xdotool key F10')
+finally:
+    if porcupine is not None:
+        porcupine.delete()
+
+    if audio_stream is not None:
+        audio_stream.close()
+
+    if pa is not None:
+        pa.terminate()
